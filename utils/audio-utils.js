2e25040ff1ba0679d3d7aac30fbc86c9cf2a1e62
atemple <templeandrew@outlook.com>
2023-01-20 18:23:31 -0500

    Fix audio paths

--- utils/audio-utils.js
+++ utils/audio-utils.js
@@ -1,10 +1,16 @@
+import path from 'node:path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { playAudioFile } from 'audic';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export const audioUtils = {
   playSound
 };
 
-function playSound(path) {
-  playAudioFile(path);
+function playSound(filePath) {
+  const audioPath = path.join(__dirname, '..', filePath);
+  playAudioFile(audioPath);
 }
 
